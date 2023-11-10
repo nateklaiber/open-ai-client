84435b1d62ccb5a98e39c3ab8c0aa859194715be
Nate Klaiber <nate@theklaibers.com>
2023-11-10 14:57:38 -0500

    Add rate limits to request

--- lib/open_ai/requests.rb
+++ lib/open_ai/requests.rb
@@ -5,6 +5,8 @@ require File.expand_path('../requests/directory', __FILE__)
 require File.expand_path('../requests/model_types', __FILE__)
 require File.expand_path('../requests/tool_types', __FILE__)
 
+require File.expand_path('../requests/rate_limit_types', __FILE__)
+
 require File.expand_path('../requests/assistants', __FILE__)
 require File.expand_path('../requests/models', __FILE__)
 require File.expand_path('../requests/threads', __FILE__)
